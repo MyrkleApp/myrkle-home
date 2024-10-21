084d075ce8b7d21fa1a803d4a545681aa66eab77
sammy <sammyoziegbe@gmail.com>
2024-10-21 05:31:24 +0100

    show under construction for devices < 920px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,46 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-import localFont from 'next/font/local'
- 
+import localFont from "next/font/local";
+import UnderConstruction from "@/components/under-construction";
+
 const myFont = localFont({
-  src: '../assets/fonts/Proxima_Nova_Font.otf',
-  display: 'swap',
-})
+  src: "../assets/fonts/Proxima_Nova_Font.otf",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Myrkle",
   description: "Myrkle: Your gateway to a decentralized world",
-  keywords: ["myrkle", "token", "nft", "asset", "crossmark", "gemwallet", "xumm", "xaman", "mint", "engine", "blockchain", "xrp", "ripple", "wallet", "crypto", "ledger"],
+  keywords: [
+    "myrkle",
+    "token",
+    "nft",
+    "asset",
+    "crossmark",
+    "gemwallet",
+    "xumm",
+    "xaman",
+    "mint",
+    "engine",
+    "blockchain",
+    "xrp",
+    "ripple",
+    "wallet",
+    "crypto",
+    "ledger",
+  ],
   robots: {
-    index: true,  // Ensuring the page is indexed
+    index: true, // Ensuring the page is indexed
     follow: true,
     nocache: true,
     googleBot: {
       index: true,
       follow: true,
-      noimageindex: true,  // Optional based on your content strategy
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      noimageindex: true, // Optional based on your content strategy
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
 };
@@ -35,7 +53,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={myFont.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <div className="app">{children}</div>
+          <div className="under-construction">
+            <UnderConstruction />
+          </div>
+        </Providers>
       </body>
     </html>
   );
